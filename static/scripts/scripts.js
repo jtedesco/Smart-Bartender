10f74fca4f90784d178558a1f5378fb0e9e6e665
Jon Tedesco <jon.c.tedesco@gmail.com>
2020-02-17 19:03:35 -0600

    Add modal & polish for drink making feedback

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -13,26 +13,37 @@ let keyHandlerFunction = function(e) {
 
   } else if (e.keyCode === 13) {
 
+    // Unbind keypresses temporarily so we can't double press
     $(window).unbind('keydown');
-    console.log('disabling keypresses...')
 
+    // Parse drink settings
     let drinkId = $('.carousel-item.active').data('drink-id');
     let drinkDelayMs = $('.carousel-item.active').data('drink-delay') * 1000;
     let startTimeMs = Date.now();
 
+    // Show the loading modal
+    let modal = $('.js-loading-bar'),
+    bar = modal.find('.progress-bar');
+    $('.progress-bar').width('0%');
+    let drinkName = $('.carousel-item.active').find('.carousel-caption h1').text();
+    $('.modal-title').text('One ' + drinkName + ' coming up!');
+    modal.modal('show');
+
+    // Animate the modal
     let intervalId = setInterval(function() {
       let elapsedMs = Date.now() - startTimeMs;
       if (elapsedMs > drinkDelayMs) {
-        // update to 100%
+        $('.progress-bar').width('100%');
       } else {
         let fractionDone = elapsedMs / drinkDelayMs;
-        // update progress bar
+        $('.progress-bar').width(Math.round(fractionDone * 100) + '%');
       }
     }, 50);
 
+    // Finished making drink callback
     let doneFunction = function() {
       console.log('Drink has been made!');
-      // clear the modal
+      modal.modal('hide');
       clearInterval(intervalId);
 
       // Re-enable key presses after a few seconds
@@ -41,6 +52,7 @@ let keyHandlerFunction = function(e) {
       }, 3000);
     };
 
+    // Make the drink!
     $.post('/make_drink/' + drinkId, doneFunction);
     return false;
   }
