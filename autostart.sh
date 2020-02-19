12fb3221fb20d3fb5776a083d1e7e4a150ae004b
Jon Tedesco <jon.c.tedesco@gmail.com>
2020-02-19 04:57:03 +0000

    Updated autostart script

--- autostart.sh
+++ autostart.sh
@@ -1,3 +1,3 @@
-sleep 10 && chromium-browser --kiosk http://localhost:5000  && unclutter -idle 0.1 -root &
+sleep 5 && chromium-browser --kiosk --disable-session-crashed-bubble http://localhost:5000  && unclutter -idle 0.1 -root &
 sudo python3 server.py
 
