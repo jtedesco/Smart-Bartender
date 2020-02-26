ee6a2c467495bb2b78b9f67f344ab38893a0c99e
Jon Tedesco <jon.c.tedesco@gmail.com>
2020-02-25 23:18:05 -0600

    Add support for garnishes

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -24,9 +24,9 @@ let keyHandlerFunction = function(e) {
     // Show the loading modal
     let modal = $('.js-loading-bar'),
     bar = modal.find('.progress-bar');
-    $('.progress-bar').width('0%');
+    modal.find('.progress-bar').width('0%');
     let drinkName = $('.carousel-item.active').find('.carousel-caption h1').text();
-    $('.modal-title').text('Making ' + drinkName + '...');
+    modal.find('.modal-title').text('Making ' + drinkName + '...');
     modal.modal('show');
 
     // Animate the modal
@@ -45,11 +45,24 @@ let keyHandlerFunction = function(e) {
       console.log('Drink has been made!');
       modal.modal('hide');
       clearInterval(intervalId);
-
-      // Re-enable key presses after a few seconds
+      
+      // Show the garnish modal
+      let secondModal = $('.garnish-reminder');
+      secondModal.modal('show');
+      let message = $('.carousel-item.active').find('.garnish').text();
+      secondModal.find('.modal-body').text(message ? message : 'Enjoy!');
+      
+      // Hide the garnish reminder in a few seconds
       setTimeout(function() {
-        $(window).keydown(keyHandlerFunction);
-      }, 3000);
+        secondModal.fadeOut('slow', function() {
+          secondModal.modal('hide');
+        });
+        // Re-enable key presses after a few seconds
+        setTimeout(function() {
+          $(window).keydown(keyHandlerFunction);
+        }, 2000);
+
+      }, 5000);
     };
 
     // Make the drink!
